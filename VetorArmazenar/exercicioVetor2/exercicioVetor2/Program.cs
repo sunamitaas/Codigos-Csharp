c1e347c7d137b0b8717ee9dcc8e1dcbff0f356f4
Sunamita Santos <123337196+sunamitaas@users.noreply.github.com>
2024-02-29 13:55:43 -0400

    Add files via upload

---
+++ VetorArmazenar/exercicioVetor2/exercicioVetor2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Exercicio 3
+// SUNAMITA SANTOS NASCIMENTO
+
+
+Console.WriteLine("Crie Vetores para Armazenar: As vogais do alfabeto, \n" +
+    "As alturas de um grupo de dez pessoas, Os nomes dos meses do ano.");
+
+//As vogais do alfabeto
+char[] vogal = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+//As alturas de um grupo de dez pessoas
+double[] altu = new double[10];
+
+//Os nomes dos meses do ano
+string[] mes = new string[] { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
+    "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
