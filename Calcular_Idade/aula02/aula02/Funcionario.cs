35afe9739dddab817d31fffd45afc0e06fafcdde
Sunamita Santos <123337196+sunamitaas@users.noreply.github.com>
2024-02-29 13:33:26 -0400

    Add files via upload

---
+++ Calcular_Idade/aula02/aula02/Funcionario.cs
@@ -0,0 +1,66 @@
+﻿
+using Microsoft.VisualBasic;
+
+public class Funcionario
+{
+    public string nome;
+    public string cpf;
+    public string email;
+    public string senha;
+    public string sexo;
+    public DateOnly dataNascimento;   //DataOnly data de nascimento é desse tipo.
+
+    // Estrutura do Metodo (funcao)
+    // O Metodo deve estar dentro da classe
+    // para Puxar função eu coloco a variavel.metodo()   
+    // Metodo void ele não retorna
+
+    // static (não precisa de um objeto para ser chamado)
+
+    public void CalcularIdade()      // receber o parametro ('tipo do parametro'  e 'variavel')
+    {
+        double idade = 0.0;
+
+        DateTime dataAtual = DateTime.Now;
+        int anoAtual = dataAtual.Year;
+        int mesAtual = dataAtual.Month;
+        int diaAtual = dataAtual.Day;
+
+        // cálculo da idade
+        idade = dataAtual.Year - dataNascimento.Year;
+
+        if (dataAtual.Month < dataNascimento.Month)
+        {
+            idade -= 1;
+
+        }
+        else if (dataAtual.Month == dataNascimento.Month)
+        {
+            if (dataAtual.Day < dataNascimento.Day)
+            {
+                idade -= 1;
+            }
+
+        }
+
+        Console.WriteLine($"A Idade de {nome} é {idade}");    // so imprimi na tela ele não retorna
+
+    }
+
+    // função para calcular dois numeros aleatorios
+     public int Calcular(int n1, int n2)
+    {
+        int soma = n1 + n2;
+
+        return soma;
+
+    }
+
+    // toda vez que eu uso retorno eu preciso especificar o tipo.
+    // o return é: toda vez que eu chamar o metodo ele deve me retornar algo,
+    // o uso do return vai fazer com que ele use a variavel retornada depois .
+
+}
+
+
+
---
+++ Calcular_Idade/aula02/aula02/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+
+Aula 02
+Sunamita Santos
+
+QUESTÃO 1 DA PROVA
+Crie uma classe e crie três instancias para ela.
+
+CLASSE - (modificador de acesso: Public, interno, protegido)
+Uma Classe define os atributos para criar um objeto é a forma. classe com letra maiuscula sempre 
+Classe - Especie
+Atributo - Gênero
+
+Metodo é uma Acão
+o Metodo ´pode receber um parametro ()
+Void é um retorno vazio
+o Metodo tem tipo de retorno e parametros
+
+*/
+
+// CRIAR UM INSTANCIA (objeto) 
+//Criando Instancia da Classe Funcionario
+
+Funcionario funcionario1 = new Funcionario();
+
+
+//1
+funcionario1.nome = "Claudia";
+funcionario1.cpf = "123.456.789.12";
+funcionario1.email = "claudia@gmail.com";
+funcionario1.senha = "123456";
+funcionario1.sexo = "feminino";
+funcionario1.dataNascimento = new DateOnly(2000, 01, 01);
+
+
+//Mostrar
+Console.WriteLine($"o Nome o 1º Funcionario é: {funcionario1.nome}");
+funcionario1.CalcularIdade();
+
+//Calcular dois numeros aleatorios
+funcionario1.Calcular(20, 10);  // você tem que criar um objeto para chamar o metodo
+
