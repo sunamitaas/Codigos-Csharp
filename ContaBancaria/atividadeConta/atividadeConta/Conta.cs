c4742f4777632598918bb83bf3680c7e6ab32085
Sunamita Santos <123337196+sunamitaas@users.noreply.github.com>
2024-02-29 13:35:17 -0400

    Add files via upload

---
+++ ContaBancaria/atividadeConta/atividadeConta/Conta.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+
+public class Conta
+{
+
+    public string id;
+    public string agencia;
+    public string numeroConta;
+    public string nomeProprietario;
+    public double saldo;
+
+    //Crie um método chamado depósito, esse método deve receber um
+    //parâmetro com o valor (valor acrescido no saldo). No entanto, não pode
+    //acrescentar no saldo valores negativos.
+
+    public double Deposito(double valorDeposito)
+    {
+
+        if (valorDeposito >= 1)
+        {
+            saldo = saldo + valorDeposito;     // ou saldo += valorDeposito;
+            Console.WriteLine($"Seu saldo de Deposito é: {valorDeposito}");
+            Console.WriteLine($"Seu saldo Final é: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Não podemos depositar número Negativos");
+        }
+
+        return saldo;
+    }
+
+    // Crie um método que realize o saque, esse método deve receber como parâmetro o valor. Obs.O saque não
+    // poder ser negativo e nem maior do que o valor permitido.
+
+    public double Saque(double saque)
+    {
+
+
+        if (saque >= 1 && saque <= saldo)
+        {
+            saldo = saldo - saque;
+            Console.WriteLine("Seu saque foi realizado com sucesso");
+            Console.WriteLine($"Seu Saldo final é: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Seu Saque não foi realizado");
+        }
+
+        return saldo;
+    }
+
+    
+    //OUTRAS FORMAS DE FAZER 
+    //Certo
+    public double Exemplo01(double valorDeposito2)
+    {
+        if (valorDeposito2 >= 1)
+        {
+            saldo += valorDeposito2;
+            
+        }
+        return saldo;
+    }
+
+
+    //Certo Tambem - se tiver 3 if então vai ter tres retornos.
+    public double Exemplo02(double valorDeposito2)
+    {
+        if (valorDeposito2 >= 1)
+        {
+            saldo += valorDeposito2;
+            return saldo;
+        }
+        else
+        {
+            return saldo;
+        }
+
+    }
+
+}   
---
+++ ContaBancaria/atividadeConta/atividadeConta/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Atividade Contas
+data: 21/02 e 28/02
+SUNAMITA SANTOS
+*/
+
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Ola aqui Vamos Acessar uma Conta");
+
+/*
+Cria uma classe que possui os seguintes atributos: Id, agencia, Nº da conta, nome
+do proprietário, saldo.
+
+Crie uma instância da conta, pedin ao usuário do sistema que informe os dados dos atributos;
+
+Crie um método chamado depósito, esse método deve receber um parâmetro com o valor(valor acrescido
+no saldo).No entanto, não pode acrescentar no saldo valores negativos.
+
+Crie um método que realize o saque, esse método deve receber como parâmetro o valor. Obs.O saque não
+poder ser negativo e nem maior do que o valor permitido.
+
+Crie uma instância da conta, pedindo ao usuário do sistema que informe os dados dos atributos
+*/
+
+
+Conta conta = new Conta();
+
+Console.WriteLine("Digite o Id: ");
+conta.id = Console.ReadLine();
+
+Console.WriteLine("Digite a Agência: ");
+conta.agencia = Console.ReadLine();
+
+Console.WriteLine("Digite Numero da Conta: ");
+conta.numeroConta = Console.ReadLine();
+
+Console.WriteLine("Digite o Nome do Proprietário: ");
+conta.nomeProprietario = Console.ReadLine();
+
+//
+
+conta.saldo = 500.00;
+Console.WriteLine($"Seu Saldo é: R${conta.saldo}");
+
+//Deposito
+
+Console.WriteLine("Digite o Valor do Deposito: ");
+double valorDeposito = Convert.ToDouble(Console.ReadLine());
+
+conta.Deposito(valorDeposito);       // chamando o método
+
+
+//Saque
+
+Console.WriteLine("Digite o Valor do Saque: ");
+double saque = Convert.ToDouble(Console.ReadLine());
+
+conta.Saque(saque);                 // estou chamando o metodo
+
+
+//criar uma lista do tipo da classe e pedir para mostrar o atributos da Conta.
+
+List<Conta> todasContas = new List<Conta>();
+
+    foreach (Conta c in todasContas)
+    {
+
+    Console.WriteLine(c.saldo);
+    Console.WriteLine(c.id);
+
+    }
+
+            
